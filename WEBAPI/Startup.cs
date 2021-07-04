ee24846ae72ab1d44e93bc58bec375fa16f3aa3e
Vivek <2421990vishal@gmail.com>
2021-07-04 12:16:12 +0530

    Pushing changes

--- WEBAPI/Startup.cs
+++ WEBAPI/Startup.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using InsolationAPI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace WEBAPI
 {
@@ -33,6 +30,25 @@ namespace WEBAPI
                                                                             .AllowAnyHeader()
                                                                             .AllowAnyMethod());
                             });
+            services.Configure<AuthOptions>(Configuration.GetSection("AuthOptions"));
+            var authOptions = Configuration.GetSection("AuthOptions").Get<AuthOptions>();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = OAuthConstants.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+             {
+                 options.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuer = true,
+                     ValidateAudience = true,
+                     ValidateLifetime = true,
+                     RequireExpirationTime = true,
+                     ValidIssuer = authOptions.Issuer,
+                     ValidAudience = authOptions.Audience,
+                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authOptions.SecureKey))
+                 };
+             });
             services.AddTransient<InsolationDb>(_ => new InsolationDb(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
